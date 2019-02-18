ef2a745cbddbd6930b89634fe14615cf5e876ceb
Elaina <elainaseki@gmail.com>
2019-02-18 06:10:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,6 +108,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info
+     return programmer_hash
      
 end
 
@@ -128,4 +129,5 @@ def adding_to_dennis
         }
      }
      programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+      return programmer_hash
 end
